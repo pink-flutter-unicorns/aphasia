43ddfec1223d15477d63e99c926c11685eaf8e1a
Dimitri Benin <dimitri.benin@gmail.com>
2020-02-01 17:45:44 +0100

    Rename to Time2Speech

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Clock2Speech',
+      title: 'Time2Speech',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Clock2Speech'),
+      home: MyHomePage(title: 'Time2Speech'),
     );
   }
 }
